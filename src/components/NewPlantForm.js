0eb06a4c1ab6da41ecb5a7503ac2d42ddf528146
ashill99 <admshng@gmail.com>
2021-01-19 15:18:51 -0500

    Done.

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,10 +1,24 @@
 import React from "react";
 
-function NewPlantForm() {
+function NewPlantForm({onSubmitClick}) {
+
+  function handleSubmitClick(event) {
+    event.preventDefault()
+    console.log(event.target.name.value)
+   const  newPlant = {
+      name: event.target.name.value, 
+      image: event.target.image.value,
+      price: event.target.price.value,
+    }
+    onSubmitClick(newPlant)
+    event.target.name.value = ""
+    event.target.image.value = ""
+    event.target.price.value = ""
+  }
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
+      <form onSubmit={handleSubmitClick}>
         <input type="text" name="name" placeholder="Plant name" />
         <input type="text" name="image" placeholder="Image URL" />
         <input type="number" name="price" step="0.01" placeholder="Price" />
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then(r => r.json())
+    .then(setPlants)
+  },[])
+
+  function handleSubmitPlant(newPlant) {
+    console.log('new plant from top')
+    console.log(newPlant)
+    const theNewPlant = fetch('http://localhost:6001/plants', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(newPlant)
+  })
+    .then(r => r.json())
+    .then(newPlantData => {
+      setPlants([...plants, newPlantData])
+    })
+  }
+
+    const results = plants.filter((p) =>  {
+    return p.name.toLowerCase().includes(search.toLowerCase())
+    })
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onSubmitClick={handleSubmitPlant}/>
+      <Search search={search} setSearch={setSearch} />
+      <PlantList plants={results}/>
     </main>
   );
 }
